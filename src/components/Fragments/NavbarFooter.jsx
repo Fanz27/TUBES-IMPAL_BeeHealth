d1b63ade6e4aef8fe5cdeb1dee8d8191bc589a9c
Fanz27 <fatihnovendi86@gmail.com>
2025-11-22 06:03:47 +0700

    Memperbaiki navbar untuk footer

--- src/components/Fragments/NavbarFooter.jsx
+++ src/components/Fragments/NavbarFooter.jsx
@@ -4,12 +4,12 @@ const NavbarFooter = () => {
     return (
         <nav className="bg-[#D9FBC5] text-gray-800 p-4 flex justify-center">
             <ul className="flex space-x-6">
-                <li><Link to="/" className="hover:underline">Home</Link></li>
-                <li><a href="#" className="hover:underline">Calculate</a></li>
-                <li><a href="#" className="hover:underline">Rekomendasi</a></li>
-                <li><a href="#" className="hover:underline">Tambah</a></li>
-                <li><a href="#" className="hover:underline">Timeline</a></li>
-                <li><a href="#" className="hover:underline">About</a></li>
+                <li><Link to="/home" className="hover:text-green-500 font-medium">Home</Link></li>
+                <li><Link to="/calculate" className="hover:text-green-500 font-medium">Calculate</Link></li>
+                <li><Link to="/Rekomendasi" className="hover:text-green-500 font-medium">Recomendasi</Link></li>
+                <li><Link to="/addMakanan" className="hover:text-green-500 font-medium">Add</Link></li>
+                <li><Link to="/timeline" className="hover:text-green-500 font-medium">Timeline</Link></li>
+                <li><Link to="/about" className="hover:text-green-500 font-medium">About</Link></li>
             </ul>
         </nav>
     )
