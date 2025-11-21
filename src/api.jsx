171e418e91542d1864c503b1a8a7f6ace6387cfb
Fanz27 <fatihnovendi86@gmail.com>
2025-11-22 05:53:31 +0700

    Memperbaiki fitur add makanan (delete,edit,add)

--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,15 @@
 import axios from "axios"
 const NGROK_URL = 'https://numbers-subessential-inarguably.ngrok-free.dev';
 export const API_URL = `${NGROK_URL}/api`;
+const token = localStorage.getItem("AuthToken");
 
 const api = axios.create({
-  baseURL: NGROK_URL,
+  baseURL: API_URL,
   headers: {
-    'ngrok-skip-browser-warning': 'true'
-  }
+    'ngrok-skip-browser-warning': 'true',
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`,
+  },
+  withCredentials: true,
 });
+export default api; 
---
+++ src/pages/RekomendasiPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Fragments/Navbar";
+import Rekomendasi from "../components/Fragments/Rekomendasi";
+
+const RekomendasiPage = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Rekomendasi/>
+        </>
+    )
+}
+
+export default RekomendasiPage;
